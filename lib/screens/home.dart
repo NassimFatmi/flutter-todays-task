39669eb4558f2b63251a7fad59be257002fd3a74
nassim gti <nassim_gti25@hotmail.fr>
2020-06-17 23:02:30 +0200

    update home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFEE5E1),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.90,
+        decoration: BoxDecoration(
+          color: Color(0xFF4F4EE9),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(60.0),
+            bottomRight: Radius.circular(60.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 20.0, top: 40.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'TODAY\'S TASK',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36.0,
+                        fontFamily: 'Oswald'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Marathon Running Prep',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontFamily: 'Oswald'),
+                  ),
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.white),
+                    child: Icon(
+                      Icons.trending_up,
+                      color: Color(0xFFF39384),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    height: 170.0,
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/workout.jpg')),
+                        borderRadius: BorderRadius.circular(25.0)),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: 50.0,
+                            height: 50.0,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.blue[900].withOpacity(0.4)),
+                            child: Icon(
+                              Icons.play_arrow,
+                              size: 45.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.radio_button_unchecked,
+                                    color: Colors.white,
+                                    size: 30.0,
+                                  ),
+                                  SizedBox(
+                                    width: 10.0,
+                                  ),
+                                  Text('Training',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w700))
+                                ],
+                              ),
+                              Text('1h a day',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w700))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 40.0),
+                            child: Text(
+                              'Daily workouts to help body to\nbecome more resilient during a marathon',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.blue[900].withOpacity(0.8)),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.radio_button_unchecked,
+                                    color: Colors.white,
+                                    size: 30.0,
+                                  ),
+                                  SizedBox(
+                                    width: 10.0,
+                                  ),
+                                  Text('Training',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w700))
+                                ],
+                              ),
+                              Text('1h a day',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w700))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 40.0),
+                            child: Text(
+                              'Daily workouts to help body to\nbecome more resilient during a marathon',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.blue[900].withOpacity(0.8)),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 10.0, vertical: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.radio_button_unchecked,
+                                    color: Colors.white,
+                                    size: 30.0,
+                                  ),
+                                  SizedBox(
+                                    width: 10.0,
+                                  ),
+                                  Text('Training',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.w700))
+                                ],
+                              ),
+                              Text('1h a day',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w700))
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 40.0),
+                            child: Text(
+                              'Daily workouts to help body to\nbecome more resilient during a marathon',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 0.0,
+          backgroundColor: Color(0xFFFEE5E1),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: SizedBox.shrink()),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.message), title: SizedBox.shrink()),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: SizedBox.shrink()),
+            
+          ]),
+    );
+  }
+}
